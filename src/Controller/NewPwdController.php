ace2de8f5529dd03fc44b0b1ab7138e5da21aa6e
Bruno Dahlem <bruno.dahlem2@gmail.com>
2023-06-23 11:23:58 +0200

    Added change pwd function

---
+++ src/Controller/NewPwdController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\NewPwdType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+
+#[Route('/user')]
+class NewPwdController extends AbstractController
+{
+    #[Route('/newpwd/{id}', name: 'app_new_pwd')]
+    public function newPwd(
+        User $user,
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher, 
+        EntityManagerInterface $entityManager
+    ): Response
+    
+    {
+        $form = $this->createForm(NewPwdType::class, $user);
+        $form->handleRequest($request);
+
+        //Check if from is Submit and valid
+        $plainPassword = $form->get('password')->getData();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $plainPassword,
+                )
+            );
+
+            // Save new password
+            $entityManager->flush();
+
+            // Redirect to validation page
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('newPwd/newPwd.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/newpassword-validated', name: 'app_new_password_validated', methods: 'GET')]
+    public function newPasswordValidated(): Response
+    {
+        return $this->render('newPwd/validatedPwd.html.twig', []);
+    }
+}
---
+++ src/Form/NewPwdType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
+class NewPwdType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les deux mots de passe saisis ne correspondent pas.',
+                'options' => [ 'attr' => [
+                    'class' => 'password-field'
+                ]],
+                'required' => true,
+                'first_options' => ['label' => 'Nouveau mot de passe'],
+                'second_options' => ['label' => 'Répétez le mot de passe'],
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "Entrez un mot de passe",
+                    ]),
+                    new Length([
+                        'min' => 12,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
+                        'max' => 4096,
+                    ]),
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
